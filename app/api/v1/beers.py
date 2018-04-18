75534241657c5b5ca1494595942d192430f25ff8
nataliya seryakova <nataliya.seryakova@gmail.com>
2018-04-18 15:30:34 -0400

    get some beer

--- app/api/v1/beers.py
+++ app/api/v1/beers.py
@@ -1,10 +1,7 @@
 from flask import jsonify, request, g, url_for, current_app
 from . import api
 
-
 @api.route('/beers/')
 def get_beers():
-beers = ["weyerbacher", "stella", "guiness"]
-    return jsonify({
-    	'beers': beers 
-    })
+	beers = ["weyerbacher", "stella", "guiness"]
+	return jsonify({'beers': beers})
--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from app.api import api_blueprint
+
 app = Flask(__name__)
+app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
 
+
 if __name__ == "__main__":
-	app.run()
+
+	app.run(debug=True)
