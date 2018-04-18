13b06a5bc48edfa0299061f6211a8dd72d570b90
nataliya seryakova <nataliya.seryakova@gmail.com>
2018-04-18 14:06:14 -0400

    first commit - app running

---
+++ app/api/v1/beers.py
@@ -0,0 +1,10 @@
+from flask import jsonify, request, g, url_for, current_app
+from . import api
+
+
+@api.route('/beers/')
+def get_beers():
+beers = ["weyerbacher", "stella", "guiness"]
+    return jsonify({
+    	'beers': beers 
+    })
